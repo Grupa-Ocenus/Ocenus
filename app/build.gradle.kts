cf13bd3bca611d6082fe6e2ecfa2a07db7bc957e
Łukasz Matecki <UL0275834@edu.uni.lodz.pl>
2024-03-09 20:29:05 +0100

    Test bazy III

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    //noinspection UseTomlInstead
     implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
+    //noinspection UseTomlInstead
     implementation("com.google.firebase:firebase-analytics")
 }
