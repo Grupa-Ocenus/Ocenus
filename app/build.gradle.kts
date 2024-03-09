44124f7eac978e5ce1e820704435c4d27fb6f3c9
Łukasz Matecki <UL0275834@edu.uni.lodz.pl>
2024-03-09 20:26:28 +0100

    Test bazy II

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.androidApplication)
+    //alias(libs.plugins.androidApplication)
     id("com.android.application")
     id("com.google.gms.google-services")
 }
@@ -42,6 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
+    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
     implementation("com.google.firebase:firebase-analytics")
 }
