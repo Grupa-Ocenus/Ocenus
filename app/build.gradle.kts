6cef97a5cdbcda337d25d3f1caf50b8fccd0d043
Pola Dorosz <ul0275880@edu.uni.lodz.pl>
2024-03-09 20:41:50 +0100

    test push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-    //alias(libs.plugins.androidApplication)
+
     id("com.android.application")
     id("com.google.gms.google-services")
+    //alias(libs.plugins.androidApplication)
 }
 
 android {
