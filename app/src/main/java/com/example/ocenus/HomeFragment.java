d2412d5e3609d1925cb3fafc7a4cb711a306af58
Pola Dorosz <ul0275880@edu.uni.lodz.pl>
2024-05-13 19:54:13 +0200

    Kafelki do wyświetlania średniej z ocen (bez obliczania średniej)

--- app/src/main/java/com/example/ocenus/HomeFragment.java
+++ app/src/main/java/com/example/ocenus/HomeFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -15,84 +16,78 @@ import androidx.fragment.app.Fragment;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
-
-import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
     private Uzytkownik uzytkownik;
-    private ListView coursesListView;
+    private GridView coursesGridView; // Changed from ListView
     private ListView gradesListView;
-    public HomeFragment(){
 
+    public HomeFragment() {
+        // Default constructor
     }
-    public HomeFragment(Uzytkownik uzytkownik){
+
+    public HomeFragment(Uzytkownik uzytkownik) {
         this.uzytkownik = uzytkownik;
     }
+
     @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        StringBuilder courses = new StringBuilder();
-        for (Kierunek course : uzytkownik.getCourses()) {
-            courses.append(course.getCourseName()).append("\n");
-        }
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-
-        Integer size = 0;
-        for(Kierunek kierunek: uzytkownik.getCourses()){
-           size += kierunek.getSubjects().size();
-        }
-        String[] s = new String[size];
-
-        int counter = 0;
+        // Create an array of subject names
+        List<String> subjectNames = new ArrayList<>();
         for (Kierunek kierunek : uzytkownik.getCourses()) {
-            for(Przedmiot przedmiot : kierunek.getSubjects()){
-                s[counter] = przedmiot.getSubjectName();
-                counter++;
+            for (Przedmiot przedmiot : kierunek.getSubjects()) {
+                subjectNames.add(przedmiot.getSubjectName());
+            }
+        }
+
+        // Set up the GridView with subject names
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                R.layout.grid_item_layout, R.id.textView, subjectNames) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView textView = view.findViewById(R.id.textView);
+                String subjectName = subjectNames.get(position);
+                if (subjectName.length() > 13) {
+                    subjectName = subjectName.substring(0, 10) + "...";
+                }
+                textView.setText(subjectName);
+                return view;
             }
 
-        }
+        };
+        coursesGridView = view.findViewById(R.id.gridView); // Use your grid view ID
+        coursesGridView.setAdapter(adapter);
 
-
-        final ArrayAdapter<String> adp = new ArrayAdapter<>(getContext(),
-                android.R.layout.simple_list_item_1, s);
-
-        coursesListView = (ListView) view.findViewById(R.id.simpleListView);
-        coursesListView.setAdapter(adp);
-//        gradesListView.setVisibility(View.INVISIBLE);
-
-        coursesListView.setOnItemClickListener(
-                (parent, view1, position, id) -> {
-                    String selectedFromList = (String) coursesListView.getItemAtPosition(position);
-                    List<Ocena> grades = new ArrayList<>();
-                    for(Ocena ocena: uzytkownik.getGrades()){
-                        if (Objects.equals(ocena.getSubjectName(), selectedFromList)){
-                            grades.add(ocena);
-                        }
-                    }
-
-
-                    String[] gradesNames = new String[grades.size()];
-
-                    int gradesCounter = 0;
-                    for (Ocena ocena : grades) {
-                        gradesNames[gradesCounter] = ocena.getName() +": "+ocena.getGrade();
-                        gradesCounter++;
-                    }
-
-
-                    final ArrayAdapter<String> gradesAdapter = new ArrayAdapter<>(getContext(),
-                            android.R.layout.simple_list_item_1, gradesNames);
-
-                    gradesListView = (ListView) view.findViewById(R.id.gradesListView);
-                    gradesListView.setAdapter(gradesAdapter);
-                    gradesListView.setVisibility(View.VISIBLE);
-                    coursesListView.setVisibility(View.INVISIBLE);
+        // Handle item click on the GridView
+        coursesGridView.setOnItemClickListener((parent, view1, position, id) -> {
+            String selectedSubject = subjectNames.get(position);
+            List<Ocena> grades = new ArrayList<>();
+            for (Ocena ocena : uzytkownik.getGrades()) {
+                if (Objects.equals(ocena.getSubjectName(), selectedSubject)) {
+                    grades.add(ocena);
                 }
-        );
+            }
+
+            String[] gradeNames = new String[grades.size()];
+            for (int i = 0; i < grades.size(); i++) {
+                Ocena ocena = grades.get(i);
+                gradeNames[i] = ocena.getName() + ": " + ocena.getGrade();
+            }
+
+            final ArrayAdapter<String> gradesAdapter = new ArrayAdapter<>(getContext(),
+                    android.R.layout.simple_list_item_1, gradeNames);
+
+            gradesListView = view.findViewById(R.id.gradesListView); // Use your list view ID
+            gradesListView.setAdapter(gradesAdapter);
+            gradesListView.setVisibility(View.VISIBLE);
+            coursesGridView.setVisibility(View.INVISIBLE);
+        });
 
         return view;
     }
