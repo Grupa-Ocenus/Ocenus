adeaf05c265409abee146e9850a79e379fbf388b
Pola Dorosz <ul0275880@edu.uni.lodz.pl>
2024-05-17 20:39:04 +0200

    powrót z ocen do śrenich a nie do edycji profilu :D

--- app/src/main/java/com/example/ocenus/StatystykiFragment.java
+++ app/src/main/java/com/example/ocenus/StatystykiFragment.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.activity.OnBackPressedCallback;
 import androidx.fragment.app.Fragment;
 
 /**
@@ -55,15 +54,10 @@ public class StatystykiFragment extends Fragment {
         }
     }
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_statystyki, container, false);
     }
-
-
-}
 }
